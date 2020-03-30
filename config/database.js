e77acc1542d4e49c24a13474de15385a4d6a51e1
Anjelica Bethea <anjelicabethea@gmail.com>
2020-03-30 08:48:19 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+module.exports = {
+
+    'url' : 'mongodb+srv://roulette:cool@cluster0-ynumo.mongodb.net/user?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'roulette'
+};
